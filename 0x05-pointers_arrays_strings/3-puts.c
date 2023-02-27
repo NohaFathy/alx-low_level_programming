ff43e0589e776455fcdd6bd8e4002b8f0852d0ef
4965 <you@example.com>
2023-02-27 12:00:00 -0800

    5 commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,13 @@
 #include "main.h"
-
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- */
+ * _puts - a C program that prints a string.
+ * @str: The string to be printed
+ **/
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
+	for (; *str != '\0'; str++)
 	{
-		_putchar(str[i]);
-	
+		_putchar(*str);
 	}
 	_putchar('\n');
 }
