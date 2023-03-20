5d253bcd55b54f20e55de7b4556b2ff57dfd4bbf
4965 <you@example.com>
2023-03-20 08:44:42 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - dog data
  * @name: name of the dog
@@ -13,13 +14,15 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}
-dog_t;
+} dog_t;
+
 /* Additional function prototypes */
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
+
 	
